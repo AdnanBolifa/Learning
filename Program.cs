99a45349138789355034ce2bee5bfac838add1c7
TheF0x <retr0f0x.f0@gmail.com>
2022-09-29 17:32:17 +0200

    edited some stuff

--- Program.cs
+++ Program.cs
@@ -8,7 +8,7 @@
             int[] msg = {    0b_00100001, 0b_01001000, 0b_01100101, 0b_01101100, 0b_01101100,
                              0b_01101111, 0b_00101100, 0b_00100000, 0b_01000111, 0b_01101001,
                              0b_01110010, 0b_01101100, 0b_01110011, 0b_00100001 };
-                             
+
             //!Correct, You nailed it!
             int[] msg2 = {  0b_00100001, 0b_01000011, 0b_01101111, 0b_01110010, 0b_01110010,
                             0b_01100101, 0b_01100011, 0b_01110100, 0b_00101100, 0b_00100000,
@@ -17,7 +17,8 @@
                             0b_00100000, 0b_01101001, 0b_01110100, 0b_00100001 };
 
             Encrypting encrypting = new Encrypting(msg2, 4);
-
+            //TODO Decrypt it to ASCII, that will be useful
+            
             Console.WriteLine("--- MESSAGE ----");
             encrypting.print();
             Console.WriteLine("--- ENCRYPTED ----");
@@ -26,6 +27,8 @@
             Console.WriteLine("--- DECRYPTED ----");
             encrypting.decrypt();
             encrypting.print();
+
+            Console.ReadLine();
         }
     }
 }
